d683836d881f90d43f6ad7fc712dfca9974fad0d
ashgit301 <67311760+ashgit301@users.noreply.github.com>
2020-10-13 15:57:39 +0530

    Update readwrite.go

--- readwrite.go
+++ readwrite.go
@@ -22,15 +22,8 @@ func New() (newObject JSON) {
 	return newObject
 }
 func AddLogs() {
-	// fmt package implements formatted
-	// I/O and has functions like Printf
-	// and Scanf
 	fmt.Printf("Writing to a file in Go lang\n")
 
-	// in case an error is thrown it is received
-	// by the err variable and Fatalf method of
-	// log prints the error message and stops
-	// program execution
 	file, err := os.Create("data/logs.txt")
 
 	if err != nil {
@@ -43,24 +36,17 @@ func AddLogs() {
 	// completed execution
 	defer file.Close()
 
-	// len variable captures the length
-	// of the string written to the file.
+	
 	len, err := file.WriteString("This is a read/write file")
 	if err != nil {
 		log.Fatalf("failed writing to file: %s", err)
 	}
-	// Name() method returns the name of the
-	// file as presented to Create() method.
 	fmt.Printf("\nFile Name: %s", file.Name())
 	fmt.Printf("\nLength: %d bytes", len)
 }
 func ReadLogs() {
 	fmt.Printf("\n\nReading a file in Go lang\n")
 	fileName := "data/logs.txt"
-
-	// The ioutil package contains inbuilt
-	// methods like ReadFile that reads the
-	// filename and returns the contents.
 	data, err := ioutil.ReadFile("data/logs.txt")
 	if err != nil {
 		log.Panicf("failed reading data from file: %s", err)
